4d821078bf4162a0507c7ee32da163993484cc0f
rajvardhanpatil2128 <rajvardhanpatil2128@gmail.com>
2026-07-14 15:50:03 +0530

    Add files via upload

---
+++ all.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int a,b,add,sub,mult,div,modulas;
+cout<<"enter first number"<<endl;
+cin>>a;
+cout<<"enter second number"<<endl;
+cin>>b;
+add=a+b;
+cout<<"add of two num is:"<<add<<endl;
+sub=a-b;
+cout<<"sub of two num is:"<<sub<<endl;
+mult=a*b;
+cout<<"mult of two num is:"<<mult<<endl;
+div=a/b;
+cout<<"div of two num is:"<<div<<endl;
+modulas=a%b;
+cout<<"modulas of two num is:"<<modulas<<endl;
+return 0;
+}
---
+++ mult.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int a,b,c;
+cout<<"enter first number"<<endl;
+cin>>a;
+cout<<"enter second number"<<endl;
+cin>>b;
+c=a*b;
+cout<<"muliti of two num is:"<<c<<endl;
+return 0;
+}
